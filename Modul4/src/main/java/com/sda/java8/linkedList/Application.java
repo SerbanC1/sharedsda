fd5ecb1dada84a75f0a5a2521c74e3787b514a6a
Serban Ciolan <Serban Ciolan>
2019-04-13 14:12:31 +0300

    Add node stub.

---
+++ Modul4/src/main/java/com/sda/java8/linkedList/Application.java
@@ -0,0 +1,10 @@
+package com.sda.java8.linkedList;
+
+public class Application {
+    public static void main(String[] args) {
+      Node node1 = new Node(20);
+      Node node2 =  new Node(30);
+      Node node3 = new Node(40);
+
+    }
+}
---
+++ Modul4/src/main/java/com/sda/java8/linkedList/Node.java
@@ -0,0 +1,12 @@
+package com.sda.java8.linkedList;
+
+public class Node {
+    private  final int value;
+    private  Node nextElement;
+
+    public Node(int value){
+        this.value = value;
+    }
+
+   }
+
