1c4c205962b4ca366081a382c459508aea553f23
Coscai Marius <coscai.marius@gmail.com>
2019-04-13 16:14:52 +0300

    Add linkedList

--- Modul4/src/main/java/com/sda/java8/linkedlist/Node.java
+++ Modul4/src/main/java/com/sda/java8/linkedlist/Node.java
@@ -1,10 +1,23 @@
 package com.sda.java8.linkedlist;
 
 public class Node {
-   private final int value;
-   private Node nextElement;
+    private final int value;
+    private Node nextElement;
 
-   public Node(int value){
-       this.value =value;
-   }
+    public Node(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+    public Node getNextElement() {
+        return nextElement;
+    }
+    public void setNextElement(Node nextElement) {
+        this.nextElement = nextElement;
+    }
+
+
+
+
 }
